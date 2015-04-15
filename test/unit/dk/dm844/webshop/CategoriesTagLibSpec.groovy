14ac4f8d196e9425665482183d335d5174b228a5
mvillumsen <mvillumsen@gmail.com>
2015-04-15 19:52:49 +0200

    Auto-generated tests for TagLibs. Still need to write tests.

---
+++ test/unit/dk/dm844/webshop/CategoriesTagLibSpec.groovy
@@ -0,0 +1,20 @@
+package dk.dm844.webshop
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(CategoriesTagLib)
+class CategoriesTagLibSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}

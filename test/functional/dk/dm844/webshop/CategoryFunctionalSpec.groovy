1227145b4f9f8caeb9ec5ecdd65df2f997210267
Anders Storm Løkke <anloe05@student.sdu.dk>
2015-05-21 14:54:30 +0200

    some more geb testing

--- test/functional/dk/dm844/webshop/CategoryFunctionalSpec.groovy
+++ test/functional/dk/dm844/webshop/CategoryFunctionalSpec.groovy
@@ -4,16 +4,28 @@
 package dk.dm844.webshop
 
 import geb.spock.GebReportingSpec
-import spock.lang.Stepwise
+import spock.lang.*
 
 @Stepwise
 class CategoryFunctionalSpec extends GebReportingSpec {
-    def "Check product listing for category 1 'Dairy'"() {
-        when: "We load Dairy category"
-        go 'http://localhost:8080/webshop/category/show/1'
 
-        then: "The title is 'Dairy'"
-        title == "Dairy"
+    @Unroll
+    def "Check product listing for category '#cat'"() {
+
+        when: "Go to front page"
+        go 'http://localhost:8080/webshop/'
+
+        then: "The title is 'Welcome to The Grocery Shop'"
+        title == "Welcome to The Grocery Shop"
+
+        when: "Locate and click on #cat category"
+        $("a", text:"$cat").click()
+
+        then:
+        title == cat
+
+        where:
+        cat << ["Dairy", "Meat", "Crisps & Candy"]
     }
 }
 

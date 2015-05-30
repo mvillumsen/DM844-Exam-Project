920839d435b1284577b76d5462054cae842293e6
Anders Storm Løkke <anloe05@student.sdu.dk>
2015-05-30 03:34:16 +0200

    differentiated logging strategy for different environments

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -95,49 +95,105 @@ grails.hibernate.pass.readonly = false
 // configure passing read-only to OSIV session by default, requires "singleSession = false" OSIV mode
 grails.hibernate.osiv.readonly = false
 
+import org.apache.log4j.DailyRollingFileAppender
 environments {
     development {
         grails.logging.jul.usebridge = true
+        log4j = {
+            appenders {
+                appender new DailyRollingFileAppender(
+                        name: 'dailyAppender',
+                        datePattern: "'.'yyyy-MM-dd",  // See the API for all patterns.
+                        fileName: "target/logs/webshop.log",
+                        layout: simple
+                )
+            }
+
+            root {
+                info 'dailyAppender'
+            }
+
+            // Example of changing the log pattern for the default console appender:
+            //
+            error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+                   'org.codehaus.groovy.grails.web.pages',          // GSP
+                   'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+                   'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+                   'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+                   'org.codehaus.groovy.grails.commons',            // core / classloading
+                   'org.codehaus.groovy.grails.plugins',            // plugins
+                   'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+                   'org.springframework',
+                   'org.hibernate',
+                   'net.sf.ehcache.hibernate'
+        }
     }
     test {
         grails.serverURL = "http://localhost:8088/webshop"
+        log4j = {
+            appenders {
+                appender new DailyRollingFileAppender(
+                        name: 'dailyAppender',
+                        datePattern: "'.'yyyy-MM-dd",  // See the API for all patterns.
+                        fileName: "target/logs/webshop.log",
+                        layout: simple
+                )
+            }
+
+            root {
+                info 'dailyAppender'
+            }
+
+            // Example of changing the log pattern for the default console appender:
+            //
+            error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+                    'org.codehaus.groovy.grails.web.pages',          // GSP
+                    'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+                    'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+                    'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+                    'org.codehaus.groovy.grails.commons',            // core / classloading
+                    'org.codehaus.groovy.grails.plugins',            // plugins
+                    'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+                    'org.springframework',
+                    'org.hibernate',
+                    'net.sf.ehcache.hibernate'
+        }
     }
     production {
         grails.logging.jul.usebridge = false
         grails.serverURL = "https://dm844group5.me"
+        log4j = {
+            appenders {
+                appender new DailyRollingFileAppender(
+                        name: 'dailyAppender',
+                        datePattern: "'.'yyyy-MM-dd",  // See the API for all patterns.
+                        fileName: "/var/log/webshop/webshop.log",
+                        layout: simple
+                )
+            }
+
+            root {
+                info 'dailyAppender'
+            }
+
+            // Example of changing the log pattern for the default console appender:
+            //
+            error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+                    'org.codehaus.groovy.grails.web.pages',          // GSP
+                    'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+                    'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+                    'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+                    'org.codehaus.groovy.grails.commons',            // core / classloading
+                    'org.codehaus.groovy.grails.plugins',            // plugins
+                    'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+                    'org.springframework',
+                    'org.hibernate',
+                    'net.sf.ehcache.hibernate'
+        }
     }
 }
 
 // log4j configuration
-import org.apache.log4j.DailyRollingFileAppender
-log4j = {
-    appenders {
-        appender new DailyRollingFileAppender(
-                name: 'dailyAppender',
-                datePattern: "'.'yyyy-MM-dd",  // See the API for all patterns.
-                fileName: "target/logs/webshop.log",
-                layout: simple
-        )
-    }
-
-    root {
-        info 'dailyAppender'
-    }
-    
-    // Example of changing the log pattern for the default console appender:
-    //
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
-}
 
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'dk.dm844.webshop.Person'
